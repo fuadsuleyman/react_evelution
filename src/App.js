3ddb2b5f0730e77e193ca50136c9fcb44405d7cc
fuad_suleymanov <fuadsuleymanov@outlook.com>
2021-09-15 23:22:30 +0400

    connect css with our component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Greet from './Components/Greet';
+import Welcome from './Components/Welcome';
 
 function App() {
   return (
     <div className="App">
+      <h1>Salam Aleykum Fuad</h1>
       <Greet/>
+      <Welcome/>
     </div>
   );
 }
--- src/Components/Greet.js
+++ src/Components/Greet.js
@@ -1,7 +1,14 @@
-import React from 'react';
+// import React from 'react';
+import './Greet.css';
+
+const Greet = () => {
+
+return (<div>
+    <div className="expense-item">September 15 2021</div>
+    <h2 className="expense-item__description">Car Inshurance</h2>
+    <div className="expense-item__price">$294.47</div>
+</div>)
 
 
-const Greet = () => <h1>Salam Aleykum Fuad</h1>
-
-
+}
 export default Greet
